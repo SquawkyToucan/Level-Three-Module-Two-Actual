20bf8eacffb46e39523ac3294ac3651248425f72
SquawkyToucan <ewbrowne743@gmail.com>
2017-10-07 14:01:54 -0700

    added the stuff

---
+++ src/intro_to_file_io/obfus.java
@@ -0,0 +1,89 @@
+package intro_to_file_io;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
+public class obfus {
+	static String key;
+	static String encryptThis;
+	obfus(String key) {
+		this.key = key;
+	}
+	public static void main(String[] args) {
+		obfus encrypt = new obfus(JOptionPane.showInputDialog("Please create a jumbler with letters a through z."));
+		System.out.println(findCipher());
+		encryptThis = JOptionPane.showInputDialog("What text would you like to encrypt?");
+		try {
+			FileWriter write = new FileWriter("src/intro_to_file_io/Encrypted Result");
+			write.write(cipher(encryptThis, findCipher()));
+			write.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public static int findCipher() {
+		//Consonants move the wheel forward by their values.
+		//Vowels move the wheel back.
+		String keyToEncryptBy = key.toLowerCase();
+		//Consonants: move up by alphabetical value
+		//Vowels: by primes in orders of two
+		/**
+		 *  - A: -2
+		 *  - E: -5
+		 *  - I: -11
+		 *  - O: -17
+		 *  - U: -23
+		 */
+		char[] letters = keyToEncryptBy.toCharArray();
+		int number = 0;
+		for(int i = 0; i < letters.length; i++) {
+			//Check if it's a consonant, vowel, or illegal character that was added by the user
+			if(letters[i] != 'a'&&letters[i] != 'e'&&letters[i] != 'i'&&letters[i] != 'o'&&letters[i] != 'u') {
+				//Consonants
+				number = number + Character.getNumericValue(letters[i]) - 9;
+				System.out.println(number);
+			}
+			else if(letters[i] == 'a'||letters[i] == 'e'&&letters[i] == 'i'&&letters[i] == 'o'&&letters[i] == 'u') {
+				//Vowels
+				if(letters[i] == 'a') {
+					number = number - 2;
+				}
+				else if(letters[i] == 'e') {
+					number = number - 5;
+				}
+				else if(letters[i] == 'i') {
+					number = number - 11;
+				}
+				else if(letters[i] == 'o') {
+					number = number - 17;
+				}
+				else if(letters[i] == 'u') {
+					number = number - 23;
+				}
+				else {
+					//Logic operator failure
+				}
+				System.out.println(number);
+			}
+			else {
+				//Do nothing, as it was wrong.
+			}
+		}
+		return Math.abs(number%26);
+	}
+	static String cipher(String msg, int shift){
+	    String s = "";
+	    int len = msg.length();
+	    for(int x = 0; x < len; x++){
+	        char c = (char)(msg.charAt(x) + shift);
+	        if (c > 'z')
+	            s += (char)(msg.charAt(x) - (26-shift));
+	        else
+	            s += (char)(msg.charAt(x) + shift);
+	    }
+	    return s;
+	}
+}
